af4ab961d7a805b4cc96390327afa2d7dc2b98d6
tami <xorud7979@gmail.com>
2023-07-10 15:47:36 +0900

    test with dummy datas

--- src/App.js
+++ src/App.js
@@ -1,35 +1,54 @@
+import excelDownload from "./libs/excel-download";
 import "./App.css";
 
 function App() {
+  const thead = ["Country", "Code", "Capital"];
+  const tableData = [
+    {
+      Country: "Korea",
+      Code: "+82",
+      Capital: "Seoul"
+    },
+    {
+      Country: "United States",
+      Code: "+1",
+      Capital: "Washington, D.C."
+    },
+    {
+      Country: "France",
+      Code: "+33",
+      Capital: "Paris"
+    }
+  ];
+  const handleExcel = () => {
+    excelDownload({
+      thead,
+      tableData,
+      sheetName: "테스트테이블",
+      fileName: "테스트테이블.xlsx"
+    });
+  };
   return (
     <>
       <table>
         <thead>
           <tr>
-            <th>Country</th>
-            <th>Code</th>
-            <th>Capital</th>
+            {thead.map((th, index) => (
+              <th key={index}>{th}</th>
+            ))}
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td>Korea</td>
-            <td>+82</td>
-            <td>Seoul</td>
-          </tr>
-          <tr>
-            <td>United States</td>
-            <td>+1</td>
-            <td>Washington, D.C.</td>
-          </tr>
-          <tr>
-            <td>France</td>
-            <td>+33</td>
-            <td>Paris</td>
-          </tr>
+          {tableData.map((row, index) => (
+            <tr key={index}>
+              <td>{row.Country}</td>
+              <td>{row.Code}</td>
+              <td>{row.Capital}</td>
+            </tr>
+          ))}
         </tbody>
       </table>
-      <button>excel download</button>
+      <button onClick={handleExcel}>excel download</button>
     </>
   );
 }
